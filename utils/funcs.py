a4fdb22be6911c0746e965fd17b6bdbf6d685bd4
sarmstrong <sarah.armstrong@seh.ox.ac.uk>
2021-08-10 11:30:04 +0000

    update utils funcs

--- utils/funcs.py
+++ utils/funcs.py
@@ -1,11 +1,13 @@
 import logging 
+import os
+from pathlib import Path
 
 """ INIT LOGGER
 the LCN_model method 'train' should output a log every 50 training iterations.
 I am setting it up here once in the main parent .py file
 logger = logging.getLogger('name_of_log') can be used to get a pre-existing logger within any object/class method
 then call logger.info(your message lala) to send some info to that log """
-
+    
 def initLogger(logger_name,output_file_name):
     logger = logging.getLogger(logger_name)
     if len(logger.handlers)>0:
@@ -18,3 +20,9 @@ def initLogger(logger_name,output_file_name):
     f_handler.setLevel(logging.INFO)
     logger.addHandler(f_handler)
     return logger
+
+def path_exists(pathname: str) -> bool:
+    p = Path(pathname)
+    return p.is_dir()
+    
+ 
